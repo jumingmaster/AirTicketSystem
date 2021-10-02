48ff9659ee811e0afacf2aba3ec3b1e02480db79
Chienheng <jumingmaster@outlook.com>
2021-10-03 00:23:18 +0800

    更新 Database.hpp 注释

--- AirTicketSystem/Database/Database.hpp
+++ AirTicketSystem/Database/Database.hpp
@@ -39,12 +39,19 @@ typedef bsoncxx::stdx::optional<mongocxx::result::insert_many> insert_many_resul
 typedef bsoncxx::stdx::optional<mongocxx::result::update> update_result;
 typedef bsoncxx::stdx::optional<mongocxx::result::delete_result> delete_result;
 
+///
+///数据库属性
+///
 namespace databaseProper {
-
+///本地数据默认地址
 const string defaultDatabaseAddr = "mongodb://localhost:27017";
+///数据库默认名字
 const string defaultDatabaseName = "AirTicketSystem";
+///航班数据集合
 const string flight_info_conn = "flight_info";
+///实名认证数据集合
 const string auth_info_conn = "auth_info";
+///用户数据集合
 const string user_info_conn = "user_info";
 };
 
@@ -58,6 +65,7 @@ const string user_info_conn = "user_info";
 /// Databases 通过 @c insertOne, @c insertMany,
 /// @c updateOne , @c updateMany, @c deleteOne, @c deleteMany, @c readOne,
 /// @c readMany 操作服务器的数据库数据
+///
 ///
 class Database {
     
@@ -79,13 +87,56 @@ public:
         Auth_Info       //实名认证数据
     };
     
+    ///
+    ///插入一个文档到本地数据库
+    ///@param info_type         文档类型
+    ///@param json                    json文档
+    ///@return             成功返回 True
+    ///
     bool insertOne(const InfoType &info_type, string json);
+    ///
+    ///插入多个文档到本地数据库
+    ///@param info_type         文档类型
+    ///@param docs                    一个json文档vector的对象
+    ///@return             成功返回 True
+    ///
     bool insertMany(const InfoType &info_type, std::vector<string> docs);
+    ///
+    ///更新一个本地数据库的文档
+    ///@param info_type         文档类型
+    ///@param filter                筛选条件（json文本）
+    ///@param json                    更新内容（json文档）
+    ///@return             成功返回 True
+    ///
     bool updateOne(const InfoType &info_type, string filter, string json);
+    ///
+    ///删除本地数据库的一个文档
+    ///@param info_type         文档类型
+    ///@param filter                筛选条件（json文本）
+    ///@return             成功返回 True
+    ///
     bool deleteOne(const InfoType &info_type, string filter);
+    ///
+    ///删除本地数据库的多个文档
+    ///@param info_type         文档类型
+    ///@param filter                筛选条件（存放json文本的vector）
+    ///@return             成功返回 True
+    ///
     bool deleteMany(const InfoType &info_type, std::vector<string> filter);
+    ///
+    ///查找本地数据库的一个文档
+    ///@param info_type         文档类型
+    ///@param filter                筛选条件（json文本）
+    ///@return             成功返回对应文档的 json 文本
+    ///
     string readOne(const InfoType &info_type, string filter);
-    std::vector<string> readMany(const InfoType &info_type, string filter);
+    ///
+    ///查找本地数据库的多个文档
+    ///@param info_type         文档类型
+    ///@param filter                筛选条件（json文档，默认为空）
+    ///@return             成功返回对应文档的vector（json文本）
+    ///
+    std::vector<string> readMany(const InfoType &info_type, string filter = "");
     
     
     
@@ -100,8 +151,14 @@ private:
     //认证数据集合
     mongocxx::collection auth_conn;
     
+    ///
+    ///返回数据库集合的所有文档
+    ///@param conn                    数据库集合
+    ///@return             成功返回一个 mongocxx::cursor 对象
+    ///
     mongocxx::cursor findAllDoc(mongocxx::collection &conn);
     
+    /*调试用*/
     //打印结果
     void printResult(insert_one_result &result) const noexcept;
     void printResult(update_result &result) const noexcept;
